c0aacca6cf90f05c47ad260cd68fa9c2d0f8cd47
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-07-07 08:24:37 +0430

    clean project

--- 01-createCronUser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#create a user for backing up data
-mysql -u root -p << EOF 
-CREATE USER if not exists 'cron_user'@'localhost' IDENTIFIED BY 'someStrongPassword';
-GRANT SELECT,LOCK TABLES, EVENT, TRIGGER, SHOW VIEW  ON *.* TO 'cron_user'@'localhost';
-FLUSH PRIVILEGES;
-EOF
--- createGitEnv.sh
+++
@@ -1,2 +0,0 @@
-GIT_REPOSITORY_URL="https://github.com/smzerehpoush/myDbBackup.git"
-git clone ${GIT_REPOSITORY_URL}
